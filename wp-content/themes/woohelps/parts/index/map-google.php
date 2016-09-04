646ed61a24c2fcb8b815e245ab3ed5ceedd2379e
Martin Sun <sunhaif@gmail.com>
2016-09-04 12:28:04 -0600

    add Google map shot cut

--- wp-content/themes/woohelps/parts/index/map-google.php
+++ wp-content/themes/woohelps/parts/index/map-google.php
@@ -20,74 +20,10 @@
     </div>
 </div>
 
-<script type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyD8FnboaQRJ4Q9Xq6QHOay2p23dtt0P75U"></script>
-<script src="<?=get_stylesheet_directory_uri()?>/js/gmaps.js"></script>
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-12 col-sm-9">
-            <div class="gmap" id="gmap">
-
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-3">
-            <div class="locations-list" id="locations-list">
-                <h3>常用地点</h3>
-                <ul id="locations-list-ul">
-
-                </ul>
-            </div>
-        </div>
+        <?php echo do_shortcode('[put_wpgm id=1]'); ?>
     </div>
 </div>
 
-<script>
-    var $ = jQuery;
-    var map;
-
-    var markers = [
-        {lat: 52.146630, lng: -106.659510, title: '餐厅'},
-        {lat: 52.141376, lng: -106.651952, title: '大超市'},
-        {lat: 52.145440, lng: -106.658522, title: '停车场'},
-        {lat: 52.142530, lng: -106.652570, title: '学校'},
-        {lat: 52.144222, lng: -106.657524, title: '市政大厅'},
-        {lat: 52.143351, lng: -106.653524, title: '喷泉广场'},
-        {lat: 52.140466, lng: -106.656514, title: '医院'}
-    ];
-
-    $('body').on('click', '.pan-to-marker', function(e) {
-        e.preventDefault();
-
-        var lat, lng;
-        var $lat = $(this).data('lat');
-        var $lng = $(this).data('lng');
-
-        lat = $lat;
-        lng = $lng;
-
-        map.setCenter(lat, lng);
-    });
-
-    $(function() {
-        map = new GMaps({
-            el: '#gmap',
-            lat: 52.146524,
-            lng: -106.671453
-        });
-
-        $.each(markers, function(index, marker) {
-            var elMarker = '<li><a href="#" class="pan-to-marker" data-lat="' + marker.lat + '" data-lng="' + marker.lng + '">' + marker.title + '</a></li>';
-
-            $(elMarker).appendTo($('#locations-list-ul'));
-            map.addMarker({
-                lat: marker.lat,
-                lng: marker.lng,
-                title: marker.title,
-                infoWindow: {
-                    content : '<p>' + marker.title + '</p>'
-                }
-            });
-        });
-    });
-</script>
