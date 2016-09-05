19445f315d1f337774c0a2f5484c1091a3f29b73
Martin Sun <sunhaif@gmail.com>
2016-09-04 23:18:08 -0600

    chang footer

--- wp-content/themes/woohelps/footer.php
+++ wp-content/themes/woohelps/footer.php
@@ -1,15 +1,3 @@
-	<div class="footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-lg-12">
-					<div class="footer-credits text-center">
-						<?php do_action('woohelps_credits'); ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
 <?php wp_footer(); ?>
 	
 </body>
