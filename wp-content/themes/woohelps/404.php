b180d7752784931a87625b27a0847a2e40ec7b45
zhiyi <wangyun@geekniu.com>
2016-09-16 15:26:58 +0800

    update: 404 页面

---
+++ wp-content/themes/woohelps/404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 404.php
+ *
+ * @author      mogita
+ * @created_by  PhpStorm
+ * @created_at  9/16/16 15:15
+ */
+get_header();
+get_header('bar');
+?>
+
+<div class="container margin-top-bigger">
+    <div class="row margin-top-100">
+        <div class="text-center">
+            <h1>
+                未找到页面
+            </h1>
+
+            <p class="margin-top-bigger">
+                您可以尝试搜索，或浏览其他内容
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
