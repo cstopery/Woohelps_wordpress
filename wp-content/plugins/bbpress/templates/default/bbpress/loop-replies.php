3282d7392bbd2f85adc5e09f40d9854060c8a4a8
zhiyi <wangyun@geekniu.com>
2016-10-04 01:06:42 +0800

    update: 论坛固定 meta 字段，包括 meetup 的必要扩展字段

--- wp-content/plugins/bbpress/templates/default/bbpress/loop-replies.php
+++ wp-content/plugins/bbpress/templates/default/bbpress/loop-replies.php
@@ -11,7 +11,48 @@
 
 <?php do_action( 'bbp_template_before_replies_loop' ); ?>
 
+<?php
+$topic_id = bbp_get_topic_id();
+$meta['date_and_time'] = get_post_meta( $topic_id, 'date_and_time', true);
+$meta['organizer'] = get_post_meta( $topic_id, 'organizer', true);
+$meta['attendee_count_limit'] = get_post_meta( $topic_id, 'attendee_count_limit', true);
+$meta['enroll_deadline'] = get_post_meta( $topic_id, 'enroll_deadline', true);
+$meta['fee'] = get_post_meta( $topic_id, 'fee', true);
+$meta['location'] = get_post_meta( $topic_id, 'location', true);
+$meta['attendee_count'] = get_post_meta( $topic_id, 'attendee_count', true);
+?>
+
 <ul id="topic-<?php bbp_topic_id(); ?>-replies" class="forums bbp-replies">
+
+	<li class="bbp-meta">
+		<div>
+			<strong>时间日期：</strong> <?=$meta['date_and_time']?>
+		</div>
+
+		<div>
+			<strong>发起人：</strong> <?=$meta['organizer']?>
+		</div>
+
+		<div>
+			<strong>限制人数：</strong> <?=$meta['attendee_count_limit']?>
+		</div>
+
+		<div>
+			<strong>报名截止日：</strong> <?=$meta['enroll_deadline']?>
+		</div>
+
+		<div>
+			<strong>费用：</strong> <?=$meta['fee']?>
+		</div>
+
+		<div>
+			<strong>地址：</strong> <?=$meta['location']?>
+		</div>
+
+		<div>
+			<strong>参加人数：</strong> <?=$meta['attendee_count']?>
+		</div>
+	</li>
 
 	<li class="bbp-header">
 
